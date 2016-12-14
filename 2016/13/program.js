5bb3979538365404d4fbdf3da2c85029b99fd61e
Patrik Simic <patrik.simic@gmail.com>
2016-12-14 18:10:40 +0100

    using BFS for day 13

--- 2016/13/program.js
+++ 2016/13/program.js
@@ -3,49 +3,7 @@ var yy = 41;
 var resx = 31;
 var resy = 39;
 var favorite = 1350;
-
-function inPath1(x, y, path)
-{
-    for(var p in path)
-    {
-        if(path[p]["x"] == x && path[p]["y"] == y)
-            return true;
-    }
-    return false;
-}
-
-function printScreen(scr, path)
-{
-    for(var r in scr)
-    {
-        var row = scr[r];
-        var line = '';
-        for(var c in row)
-        {
-            if(inPath1(c, r, path))
-                line += '0';
-            else
-                line += row[c];
-        }
-        console.log(line);
-    }
-}
-
-function makeScreen(x, y)
-{
-    var ret = [];
-
-    for(var i = 0; i < y; i++)
-    {
-        var row = [];
-        for(var j = 0; j < x; j++)
-        {
-            row.push('.');
-        }
-        ret.push(row);
-    }
-    return ret;
-}
+var walkLimit = 50;
 
 function isEven(n) 
 {
@@ -57,7 +15,6 @@ function dec2bin(dec)
     return (dec >>> 0).toString(2);
 }
 
-//x*x + 3*x + 2*x*y + y + y*y
 function isOpenSpace(x, y, favorite)
 {
     var res = x*x + 3*x + 2*x*y + y + y*y;
@@ -74,239 +31,211 @@ function isOpenSpace(x, y, favorite)
     return false;
 }
 
-function createMaze(scr, favorite)
+var graph = [];
+
+function createAdj(x, y, xx, yy)
 {
-    var ret = [];
-    for(var i = 0; i < scr.length; i++)
-    {
-        var row = scr[i];
-        var retRow = [];
-        for(var j = 0; j < row.length; j++)
-        {
-            var os = isOpenSpace(j,i, favorite);
-            if(j == 1 && i == 1)
-                retRow.push('O');
-            else if(j == resx && i == resy)
-                retRow.push('O');
-            else
-            {
-                if(!os)
-                    retRow.push('#');
-                else
-                    retRow.push('.'); 
-            }
-        }
-        ret.push(retRow);
-    }
-    return ret;
-}
-
-function inPath(path, obj)
-{
-    for(var i in path)
-        if(obj["x"] == path[i]["x"] && obj["y"] == path[i]["y"])
-            return true;
-    return false;
-}
-
-function cutPath(path)
-{
-    var obj = path[path.length-1];
-    var ret = [];
-    for(var i in path)
-    {
-        ret.push(path[i]);   
-        if(obj["x"] == path[i]["x"] && obj["y"] == path[i]["y"])
-        {
-            return ret;
-        }    
-    }
-    return ret;
-}
-
-function inDirection(ch, dir)
-{
-    for(var d in dir)
-    {
-        if(ch === dir[d])
-            return true;
-    }
-    return false;
-}
-
-function disallowedMoves(path)
-{
-    var obj = path[path.length-1];
-    var dis = obj["dir"];
-
-    var x = obj["x"];
-    var y = obj["y"];
-
+    var adj = [];
     var xplus1 = x + 1;
     var yplus1 = y + 1;
     var xminus1 = x -1;
     var yminus1 = y - 1;
-    
-    
-    if(!inDirection("D", dis))
-    {
-         if(!(yplus1 < yy && isOpenSpace(x, yplus1, favorite)))
-            dis += "D";
-    }
-    if(!inDirection("L", dis))
-    {
-        if(!(xminus1 >= 0 && isOpenSpace(xminus1, y, favorite)))
-            dis += "L";
-    }   
-    if(!inDirection("U", dis))
-    {
-        if(!(yminus1 >= 0 && isOpenSpace(x, yminus1, favorite)))
-            dis += "U";
-    }
-    if(!inDirection("R", dis))
-    {
-        if(!(xplus1 < xx && isOpenSpace(xplus1, y, favorite)))
-            dis += "R";
-    }
 
-    path[path.length-1]["dir"] = dis;
-    return path;
+    if(yplus1 < yy && isOpenSpace(x, yplus1, favorite))
+        adj.push({ "x" : x, "y" : yplus1 });
+    if(xminus1 >= 0 && isOpenSpace(xminus1, y, favorite))
+        adj.push({ "x" : xminus1, "y" : y });
+    if(yminus1 >= 0 && isOpenSpace(x, yminus1, favorite))
+        adj.push({ "x" : x, "y" : yminus1 });
+    if(xplus1 < xx && isOpenSpace(xplus1, y, favorite))
+        adj.push({ "x" : xplus1, "y" : y });
+
+    return adj;
 }
 
-function moveLeft(path)
-{
-    var obj = path[path.length-1];
-    var x = obj["x"];
-    var y = obj["y"];
-    var dir = obj["dir"];
-    dir += "L";
-    path[path.length-1]["dir"] = dir;
-
-    path.push({ "x" : x-1, "y" : y, "dir" : 'R'});
-    return path;
-}
-
-function moveRight(path)
-{
-    var obj = path[path.length-1];
-    var x = obj["x"];
-    var y = obj["y"];
-    var dir = obj["dir"];
-    dir += "R";
-    path[path.length-1]["dir"] = dir;
-
-    path.push({ "x" : x+1, "y" : y, "dir" : 'L'});
-    return path;
-}
-
-function moveUp(path)
-{
-    var obj = path[path.length-1];
-    var x = obj["x"];
-    var y = obj["y"];
-    var dir = obj["dir"];
-    dir += "U";
-    path[path.length-1]["dir"] = dir;
-
-    path.push({ "x" : x, "y" : y-1, "dir" : 'D'});
-    return path;
-}
-
-function moveDown(path)
-{
-    var obj = path[path.length-1];
-    var x = obj["x"];
-    var y = obj["y"];
-    var dir = obj["dir"];
-    dir += "D";
-    path[path.length-1]["dir"] = dir;
-
-    path.push({ "x" : x, "y" : y+1, "dir" : 'U'});
-    return path;
-}
-
-function cleanPath(path)
+function createGraph(xx, yy)
 {
     var ret = [];
-    for(var p in path)
+    for(var x = 0; x < xx; x++)
     {
-        if(path[p]["dir"].length < 4)
-            ret.push(path[p]);
+        for(var y = 0; y < yy; y++)
+        {
+            if(isOpenSpace(x, y, favorite))
+            {
+                var node = {"x" : x, "y" : y};    
+                var adj = createAdj(x, y, xx, yy);
+                ret.push({ "node" : node, "adj" : adj, "distance" : -1, "parent" : {} });
+            }
+        }
     }
     return ret;
 }
 
-function traverse(path)
+function printGraph (g) 
 {
-    var obj = path[path.length-1];
-    var x = obj["x"];
-    var y = obj["y"];
-
-    if(x == resx && y == resy)
+    for(var n in g)
     {
-        for(var p in path)
-            console.log(path[p]);
-        console.log(path.length-1);
-        return;
+        console.log("node -> x : " + g[n]["node"]["x"] + "; y : " + g[n]["node"]["y"]);
+        var adj = g[n]["adj"];
+        for(var a in adj)
+            console.log(adj[a]);
     }
-    
-    path = disallowedMoves(path);
-    var dir = path[path.length-1]["dir"];
-    
-    var i = 1000;
-    while(i > 0)
-    {
-        if(!inDirection('D', dir))
-        {
-            path = moveDown(path);
-        }    
-        else if(!inDirection('R', dir))
-        {
-            path = moveRight(path);
-        }
-        else if(!inDirection('L', dir))
-        {
-            path = moveLeft(path);
-        }
-        else if(!inDirection('U', dir))
-        {
-            path = moveUp(path);
-        }
-        else
-        {
-            var k = path.length-1;
-            while(k > 0)
-            {
-                if(path[k]["dir"].length >= 4)
-                {
-                    path.pop();
-                    k--;
-                }
-                else
-                    k = 0;
-            }
-
-        }
-        
-        path = cutPath(path);
-        path = disallowedMoves(path);
-        dir = path[path.length-1]["dir"];
-
-        var p = path[path.length-1];
-        if(p["x"] == resx && p["y"] == resy)
-            return path;
-
-        i--;
-    }
-    return path;
 }
 
-var scr = makeScreen(xx, yy);
-scr = createMaze(scr, favorite);
+function printScreen(scr)
+{
+    for(var r in scr)
+    {
+        var row = scr[r];
+        var line = '';
+        for(var c in row)
+            line += row[c];
+        console.log(line);
+    }
+}
 
-var path = [{"x" : 1, "y" : 1, dir : ""}];
-path = traverse(path);
-printScreen(scr, path);
-console.log(path.length-1);
+function inPath(path, x, y)
+{
+    for(var p in path)
+    {
+        var xp = path[p]["x"];
+        var yp = path[p]["y"];
+        if(xp == x && yp == y)
+            return true;
+    }
+    return false;
+}
 
+function makeScreen(x, y, path)
+{
+    var ret = [];
 
+    for(var i = 0; i < y; i++)
+    {
+        var row = [];
+        for(var j = 0; j < x; j++)
+        {
+            var os = isOpenSpace(j,i, favorite);
+            if(!os)
+                row.push('#');
+            else
+            {
+                if(inPath(path, j, i))
+                    row.push('0');
+                else
+                    row.push('.');
+            }
+        }
+        ret.push(row);
+    }
+    return ret;
+}
+
+function getNodeFromGraph(g, x, y)
+{
+    for(var n in g)
+    {
+        var node = g[n]["node"];
+        if(node["x"] == x && node["y"] == y)
+            return g[n];
+    }
+    return {};
+}
+
+function BFS(g, xroot, yroot)
+{
+    var queue = [];
+    var nodeRoot = getNodeFromGraph(g, 1, 1);
+    nodeRoot["distance"] = 0;
+    nodeRoot["parent"] = {};
+
+    queue.push(nodeRoot);
+
+    while(queue.length > 0)
+    {
+        var nodeCurrent = queue.shift();
+        var currentDist = nodeCurrent["distance"];
+        var adj = nodeCurrent["adj"];
+        for(var a in adj)
+        {
+            var xa = adj[a]["x"];
+            var ya = adj[a]["y"];
+            var nodeTest = getNodeFromGraph(g, xa, ya);
+            if(nodeTest["node"]["x"] == resx && nodeTest["node"]["y"] == resy)
+                return {"distance" : currentDist+1, "queue" : queue }
+
+            if(nodeTest["distance"] < 0)
+            {
+                nodeTest["distance"] = currentDist + 1;
+                nodeTest["parent"] = nodeCurrent;
+                queue.push(nodeTest);
+            }
+        }
+    }
+}
+
+function BFS2(g, xroot, yroot, walkLimit)
+{
+    var nodesVisited = 0;
+    var queue = [];
+    var nodeRoot = getNodeFromGraph(g, 1, 1);
+    nodeRoot["distance"] = 0;
+    nodeRoot["parent"] = {};
+
+    queue.push(nodeRoot);
+
+    while(queue.length > 0)
+    {
+        var nodeCurrent = queue.shift();
+        var currentDist = nodeCurrent["distance"];
+        
+        if(currentDist <= walkLimit)
+        {
+            var adj = nodeCurrent["adj"];
+            for(var a in adj)
+            {
+                var xa = adj[a]["x"];
+                var ya = adj[a]["y"];
+                var nodeTest = getNodeFromGraph(g, xa, ya);
+                if(nodeTest["distance"] < 0)
+                {
+                    nodeTest["distance"] = currentDist + 1;
+                    nodeTest["parent"] = nodeCurrent;
+                    queue.push(nodeTest);
+                    nodesVisited++;
+                }
+            }
+        }
+    }
+    console.log(nodesVisited);
+}
+
+function createPath(node)
+{
+    var ret = [];
+    var x = node["node"]["x"];
+    var y = node["node"]["y"];
+    ret.push({"x": x, "y": y});
+
+    var p = node["parent"];
+    while(p["node"] !== undefined)
+    {
+        var x = p["node"]["x"];
+        var y = p["node"]["y"];
+        ret.push({"x": x, "y": y});        
+        p = p["parent"];    
+    }
+    return ret;
+}
+
+graph = createGraph(xx, yy);
+var obj = BFS(graph, 1, 1);
+
+var path = createPath(obj["queue"].shift());
+var scr = makeScreen(xx, yy, path);
+printScreen(scr);
+
+console.log(obj["distance"]);
+
+graph2 = createGraph(xx, yy);
+BFS2(graph2, 1, 1, walkLimit);
