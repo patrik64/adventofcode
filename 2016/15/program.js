9c6a0ca196d35355ea6d2e9c0939cec3c8d44abc
Patrik Simic <patrik.simic@gmail.com>
2016-12-15 06:49:21 +0100

    rename

--- 2016/15/program.js
+++ 2016/15/program.js
@@ -5,30 +5,24 @@ var arr = input.split('\n');
 
 function parse(str)
 {
-    var disknr = '';
-    var positions = '';
-    var position = '';
-    var param1 = '';
-    var param2 = '';
-    var param3 = '';
-    var pos = 6;
-
-    disknr = Number(str[pos]);
+    var ps = '';
+    var p = '';
+    
     pos =12;
     while(str[pos] != ' ')
     {
-        positions += str[pos];
+        ps += str[pos];
         pos++;
     }
 
     pos = str.length-2;
-    position = str[pos];
+    p = str[pos];
 
-    var ret = { "disknr" : disknr, "ps" : Number(positions), "p" : Number(position) };
+    var ret = { "ps" : Number(ps), "p" : Number(p) };
     return ret;
 }
 
-function answerPuzzle(arr, i)
+function isUnlocked(arr, i)
 {
     for(var x in all)
     {
@@ -36,7 +30,6 @@ function answerPuzzle(arr, i)
         var obj = all[x];
         var p = obj["p"];
         var ps = obj["ps"];
-        var check = (p+i) % ps;
         if(((p+i) % ps) != 0)
             return false;
     }
@@ -49,23 +42,14 @@ for (var i in arr)
     var obj = parse(arr[i]);
     all.push(obj);
 }
+
+//problem 1
 var i = 0;
-var ret = answerPuzzle(all, i);
-while(!ret)
-{
-    ret = answerPuzzle(all, i);
-    i++;
-}
-console.log(i-1);
+while(!isUnlocked(all, i)) i++;
+console.log(i);
 
 //problem 2
-all.push({ "disknr" : 7, "ps" : 11, "p" : 0 });
-
+all.push({ "ps" : 11, "p" : 0 });
 var i = 0;
-var ret = answerPuzzle(all, i);
-while(!ret)
-{
-    ret = answerPuzzle(all, i);
-    i++;
-}
-console.log(i-1);
+while(!isUnlocked(all, i)) i++;
+console.log(i);
