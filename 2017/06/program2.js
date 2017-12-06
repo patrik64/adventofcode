f30bfa0b7078c6a2e476bf3ccffdd13968f276cc
Patrik Simic <patrik.simic@gmail.com>
2017-12-06 07:40:20 +0100

    day 6

---
+++ 2017/06/program2.js
@@ -0,0 +1,78 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('Day6.in', 'utf8');
+var arr = input.split('\n');
+
+function getMax(ar)
+{
+    var sa = ar.slice(0);
+    sa.sort(function(a, b){return a-b});
+    return sa[sa.length-1];
+}
+
+function getRotation(ar)
+{
+    var ret = ar.slice(0);
+    var mxIdx = 0;
+    var mx = getMax(ar);
+    for(var i = 0; i < ar.length; i++)
+    {
+        if(ar[i] == mx)
+        {
+            mxIdx = i;
+            ret[mxIdx] = 0;
+            break;
+        }
+    }
+    
+    var rIdx = mxIdx + 1;
+    while(mx > 0)
+    {
+        if(rIdx >= ar.length)
+            rIdx = 0;
+        ret[rIdx] += 1;
+        rIdx++;
+        mx--;
+    }
+    return ret;
+}
+
+function getRotationPos(ar, coll)
+{
+    for(var j = 0; j < coll.length; j++)
+    {
+        var test = coll[j];
+        var bFound = true;
+        for(var i = 0; i < ar.length; i++)
+        {
+            if(test[i] != ar[i])
+                bFound = false;
+        }
+        if(bFound) 
+            return j;
+    }
+    return -1;
+}
+
+var sum = 0;
+var line = arr[0];
+var arrN = line.split('\t').map(function(item) {
+    return Number(item);
+});
+
+var rotations = [];
+rotations.push(arrN);
+var rot = getRotation(arrN);
+rotations.push(rot);
+var matchPos = -1;
+for(;;)
+{
+    rot = getRotation(rot);
+    matchPos = getRotationPos(rot, rotations);
+    if(matchPos > 0)
+        break;
+    else
+        rotations.push(rot);
+}
+
+console.log(rotations.length - matchPos);
