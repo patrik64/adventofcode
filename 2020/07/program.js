24e0269607d9deeb1afeb78a591ec0c295277a92
Patrik Simic <patrik.simic@gmail.com>
2020-12-07 15:17:18 +0100

    day 7

---
+++ 2020/07/program.js
@@ -0,0 +1,113 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('Day7.in', 'utf8');
+var arr = input.split('\n');
+
+function parse(str)
+{
+    let idx = 0;
+    let val = -1;
+
+    let bag = '';
+    [bag, idx] = getBagName(str, idx);
+
+    if(str[idx] === ' ')
+        idx += 14;
+
+    val = str[idx];
+
+    if(val === 'n') {
+        return { "bagColor" : bag, "contains": []}
+    } else {
+        let cont = [];
+        idx += 2;
+        let cbag = '';
+        [cbag, idx] = getBagName(str, idx);
+        idx++;
+        cont.push(cbag);
+        idx = hasMoreBags(str, idx);
+        while (idx > 0 ) {
+            let cbag = '';
+            [cbag, idx] = getBagName(str, idx);
+            cont.push(cbag);
+            idx = hasMoreBags(str, idx);
+        }
+        return { "bagColor" : bag, "contains": cont}
+    }
+}
+
+function hasMoreBags(str, idx) {
+    while(str[idx] !== '.')
+    {
+        if(str[idx] === ',') {
+            idx += 4;
+            return idx;
+        }
+        idx++;
+    }
+    return -1;
+}
+
+function getBagName(str, idx) {
+    var attr = '';
+    var color = '';
+
+    while(str[idx] !== ' ')
+    {
+        attr += str[idx];
+        idx++;
+    }
+    attr.trim();
+    idx ++;
+
+    while(str[idx] != ' ')
+    {
+        color += str[idx];
+        idx++;
+    }
+    color.trim();
+
+    let bag = attr + '-' + color;
+    return [bag, idx];
+}
+
+function isInArray(value, array) {
+    return array.indexOf(value) > -1;
+}
+
+function containsBag(bags, inputBag, testBag) {
+    for (let x in bags) {
+        if(x === testBag && x!== inputBag) {
+            if (isInArray(inputBag, bags[x])) {
+                return true;
+            }
+            for (let y in bags[x] ) {
+                let t = bags[x][y]
+                if(containsBag(bags, inputBag, t))
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+var bags = {}
+
+for (let i in arr) {
+    let line = arr[i];
+    let obj = parse(line);
+    bags[obj.bagColor] = obj.contains;
+}
+
+let inputBag = "shiny-gold";
+
+let count = 0;
+for (let x in bags) {
+    let bag = bags[x];
+    if(x !== inputBag) {
+        if(containsBag(bags, inputBag, x))
+            count++;
+    }
+}
+
+console.log(count)
---
+++ 2020/07/program2.js
@@ -0,0 +1,112 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('Day7.in', 'utf8');
+var arr = input.split('\n');
+
+function parse(str)
+{
+    let idx = 0;
+    let val = -1;
+
+    let bag = '';
+    [bag, idx] = getBagName(str, idx);
+
+    if(str[idx] === ' ')
+        idx += 14;
+
+    val = str[idx];
+
+    if(val === 'n') {
+        return { "bagColor" : bag, "contains": []}
+    } else {
+        let cont = [];
+        idx += 2;
+        let cbag = {};
+        let bc = '';
+        [bc, idx] = getBagName(str, idx);
+        idx++;
+        cbag = { 'bagColor': bc, 'val' : Number(val) }
+        cont.push(cbag);
+        idx = hasMoreBags(str, idx);
+        while (idx > 0 ) {
+            val = str[idx];
+            idx++;
+            idx++;
+            let cbag = {};
+            let bc = '';
+            [bc, idx] = getBagName(str, idx);
+            cbag = { 'bagColor': bc, 'val' : Number(val) }
+            cont.push(cbag);
+            idx = hasMoreBags(str, idx);
+        }
+        return { "bagColor" : bag, "contains": cont}
+    }
+}
+
+function hasMoreBags(str, idx) {
+    while(str[idx] !== '.')
+    {
+        if(str[idx] === ',') {
+            idx+=2;
+            return idx;
+        }
+        idx++;
+    }
+    return -1;
+}
+
+function getBagName(str, idx) {
+    var attr = '';
+    var color = '';
+
+    while(str[idx] !== ' ')
+    {
+        attr += str[idx];
+        idx++;
+    }
+    attr.trim();
+    idx ++;
+
+    while(str[idx] != ' ')
+    {
+        color += str[idx];
+        idx++;
+    }
+    color.trim();
+
+    let bag = attr + '-' + color;
+    return [bag, idx];
+}
+
+function isInArray(value, array) {
+    return array.indexOf(value) > -1;
+}
+
+function getBagCount(b, bags) {
+    let res = 0;
+    let arr = bags[b];
+    for(let x in arr) {
+        let bc = arr[x].bagColor;
+        let val = arr[x].val;
+        val += val * getBagCount(bc, bags);
+        res += val;
+    }
+    return res;
+
+}
+
+var bags = {}
+
+for (let i in arr) {
+    let line = arr[i];
+    let obj = parse(line);
+    bags[obj.bagColor] = obj.contains;
+}
+
+let inputBag = 'shiny-gold';
+
+let count = 0;
+
+count += getBagCount('shiny-gold', bags);
+
+console.log(count);
