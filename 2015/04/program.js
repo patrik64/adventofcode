3b778a4c79903eecc52d68dd9707bb16424007f1
Patrik Simic <patrik.simic@gmail.com>
2020-12-15 13:55:40 +0100

    Update program.js

--- 2015/04/program.js
+++ 2015/04/program.js
@@ -1,13 +1,11 @@
-var md5 = require('./md5');
-var input = "iwrupvqb";
-var counter = 1;
-var password = '';
-for(;;)
-{
-    var test = input + counter.toString();
-    var hash = md5.md5(test);
-    if(hash.indexOf('00000') == 0)
-    {
+let md5 = require('./md5');
+let input = "iwrupvqb";
+let counter = 1;
+let password = '';
+for(;;) {
+    let test = input + counter.toString();
+    let hash = md5.md5(test);
+    if(hash.indexOf('00000') == 0) {
         console.log(counter);
         process.exit();
     }    
