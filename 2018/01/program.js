7fd7faaa2e79778f16c87e25db67aa31526fbcca
Patrik Simic <patrik.simic@gmail.com>
2021-12-01 13:55:13 +0100

    Update program.js

--- 2018/01/program.js
+++ 2018/01/program.js
@@ -3,15 +3,13 @@ let fs = require('fs');
 let input = fs.readFileSync('Day1.in', 'utf8');
 let arr = input.split('\n');
 
-function parse(str)
-{
+function parse(str) {
    let ret = parseInt(str);
    return ret;
 }
 
 let sum = 0;
-for(let i in arr)
-{
+for(let i in arr) {
     let n = parse(arr[i]);
     sum = sum + n;
 }
